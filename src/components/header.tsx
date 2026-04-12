d591f099c283e6d282688a762470c1c6241711b7
Volodymyr <117123903+dudychvo@users.noreply.github.com>
2026-04-12 18:12:41 +0100

    refactor: update Header component structure and improve link styling

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,4 @@
-import Image from 'next/image';
-
 const links = [
-  { name: 'Home', href: '#home' },
   { name: 'About me', href: '#about' },
   { name: 'Services', href: '#services' },
   { name: 'Contact me', href: '#contact' },
@@ -9,29 +6,33 @@ const links = [
 
 export const Header = () => {
   return (
-    <>
-      <div
-        id='home'
-        className='h-18 bg-black flex items-center justify-between px-16 rounded-bl-3xl rounded-br-3xl '
-      >
-        <Image
-          src={'/logo.svg'}
-          alt={'Website logo'}
-          width={52}
-          height={52}
-          loading='eager'
-        />
-        <ul className='text-white text-[24px] font-semibold flex flex-row gap-10'>
+    <header className='w-full max-w-7xl mx-auto bg-black rounded-b-3xl px-8 md:px-16 py-5 flex items-center justify-between border-b-4 border-[#00c6ae] mb-8'>
+      <div className='flex justify-start'>
+        <a className='font-mono font-black text-2xl md:text-3xl tracking-tighter transition-transform duration-300 hover:scale-105'>
+          <span className='text-[#00c6ae]'>&lt;</span>
+          <span className='text-white hover:text-[#FFD700] transition-colors duration-300'>
+            VD
+          </span>
+          <span className='text-[#00c6ae]'> /&gt;</span>
+        </a>
+      </div>
+      <nav aria-label='Main menu'>
+        <ul className='hidden md:flex flex-row gap-8 lg:gap-12 text-white text-xl md:text-[24px] font-bold'>
           {links.map((link) => (
             <li
               key={link.name}
-              className='inline-block cursor-pointer transition-all duration-300 ease-in-out hover:text-[#00c6ae] hover:scale-110 '
+              className='min-w-30 text-center flex justify-center'
             >
-              <a href={link.href}>{link.name}</a>
+              <a
+                href={link.href}
+                className='inline-block transition-transform duration-300 ease-out hover:text-[#00c6ae] hover:scale-110 origin-center'
+              >
+                {link.name}
+              </a>
             </li>
           ))}
         </ul>
-      </div>
-    </>
+      </nav>
+    </header>
   );
 };
