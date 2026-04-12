256d6fc9255810d60ff56d096701cd5040b9719c
Volodymyr <117123903+dudychvo@users.noreply.github.com>
2026-04-12 18:12:11 +0100

    fix: correct import paths for components in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
-import { Header } from '@/components/header';
-import { Hero } from '@/components/hero';
-import { About } from '@/components/about';
-import { Services } from '@/components/services';
-import { Contact } from '@/components/contact';
-import { Footer } from '@/components/footer';
+import { CustomCursor } from '@/components/CustomCursor';
+
+import { Header } from '@/components/Header';
+import { Hero } from '@/components/Hero';
+import { About } from '@/components/About';
+import { Services } from '@/components/Services';
+import { Contact } from '@/components/Contact';
+import { Footer } from '@/components/Footer';
 
 export default function Home() {
   return (
-    <div className='flex flex-col min-h-screen w-300 mx-auto '>
+    <div className='flex flex-col min-h-screen w-300 mx-auto'>
+      <CustomCursor />
       <Header />
       <Hero />
       <About />
